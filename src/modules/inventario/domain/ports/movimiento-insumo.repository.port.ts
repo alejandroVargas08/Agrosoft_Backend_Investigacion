0232a3817c3bdb1d4d6d0841ae39fb0d79a01892
Juan Valencia <juandavidpv2006@gmail.com>
2026-08-11 22:51:53 -0500

    hice cambios en el ports de movimientos

--- src/modules/inventario/domain/ports/movimiento-insumo.repository.port.ts
+++ src/modules/inventario/domain/ports/movimiento-insumo.repository.port.ts
@@ -1,9 +1,11 @@
 import { MovimientoInsumo } from '../entities/movimiento-insumo.entity';
+import { Insumo } from '../entities/insumo.entity';
 
 export interface MovimientoInsumoRepositoryPort {
     buscarPorId(id: number): Promise<MovimientoInsumo | null>;
     buscarPorInsumoId(insumoId: number): Promise<MovimientoInsumo[]>;
     guardar(movimiento: MovimientoInsumo): Promise<MovimientoInsumo>;
+    guardarConInsumo(insumo: Insumo, movimiento: MovimientoInsumo): Promise<MovimientoInsumo>;
 }
 
 export const MOVIMIENTO_INSUMO_REPOSITORY_PORT = Symbol('MOVIMIENTO_INSUMO_REPOSITORY_PORT');
