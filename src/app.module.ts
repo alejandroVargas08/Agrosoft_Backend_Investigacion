899916f37c2d5d15a52ed3f50ae27dc6d7e5970c
Juan Valencia <juandavidpv2006@gmail.com>
2026-08-25 11:09:10 -0500

    solito solitoooo agregue JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,7 +32,7 @@ import { WikiEpasModule } from './modules/wiki-epas/wiki-epas.module';
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       schema: 'public',
-      synchronize: false,
+      synchronize: true,
       logging: true,
     }),
     TerritorioModule,
